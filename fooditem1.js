e0c2f6e6c410ca1d6a09eac495687c4820b8a141
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2024-03-30 23:31:17 +0530

    Add new file for project

---
+++ fooditem1.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Paneer Tikka Pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 150,
+    img: 'order/pizza1.png',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Tandoori Chicken Pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 149,
+    img: 'order/pizza2.png',
+    quantity: 1
+},
+{
+    id: 3,
+    name: ' Masala Pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 125,
+    img: 'order/pizza3.png',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Uttapam Pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 250,
+    img: 'order/pizza4.jpeg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Keema Pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 199,
+    img: 'order/pizza5.jpeg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Onion Pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 99,
+    img: 'order/pizza6.png',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Neapolitan Pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 139,
+    img: 'order/pizza1.png',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Sicilian Pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 219,
+    img: 'order/pizza2.png',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'California Pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 145,
+    img: 'order/pizza3.png',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Paper Dosa',
+    category : 'dosa',
+    rating : 4.3,
+    price: 149,
+    img: 'order/dosa1.jpeg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Rava Dosa',
+    category : 'dosa',
+    rating : 4.3,
+    price: 110,
+    img: 'order/dosa2.jpeg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Podi Dosa',
+    category : 'dosa',
+    rating : 4.3,
+    price: 169,
+    img: 'order/dosa3.png',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Moong Dal Dosa',
+    category : 'dosa',
+    rating : 4.3,
+    price: 200,
+    img: 'order/dosa4.png',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Masala Dosa',
+    category : 'dosa',
+    rating : 4.3,
+    price: 180,
+    img: 'order/dosa5.png',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Coconut Dosa',
+    category : 'dosa',
+    rating : 4.3,
+    price: 120,
+    img: 'order/dosa1.jpeg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 250,
+    img: 'order/panner1.png',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 349,
+    img: 'order/panner2.png',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Paneer Butter',
+    category : 'paneer',
+    rating : 4.3,
+    price: 230,
+    img: 'order/panner3.webp',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Paneer Do Pyaza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 379,
+    img: 'order/panner4.png',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'cheese Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 180,
+    img: 'order/panner5.webp',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Paneer Lababdar',
+    category : 'paneer',
+    rating : 4.3,
+    price: 149,
+    img: 'order/panner6.png',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Shahi Paneer',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 245,
+    img: 'order/panner2.png',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Thali',
+    category : 'chiense',
+    rating : 4.3,
+    price: 180,
+    img: 'order/thali3.jpeg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Sandwitch',
+    category : 'chiense',
+    rating : 4.3,
+    price: 80,
+    img: 'order/sand1.jpeg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Burger',
+    category : 'chiense',
+    rating : 4.3,
+    price: 79,
+    img: 'order/burger1.jpeg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Coffee',
+    category : 'chiense',
+    rating : 4.3,
+    price: 49,
+    img: 'order/coffe1.jpeg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Khasta Kachori',
+    category : 'chiense',
+    rating : 4.3,
+    price: 39,
+    img: 'order/fast2.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Ice Cream',
+    category : 'chiense',
+    rating : 4.3,
+    price: 59,
+    img: 'order/ice4.webp',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Pizza',
+    category : 'chiense',
+    rating : 4.3,
+    price: 89,
+    img: 'order/pizza6.png',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Chole Bhature',
+    category : 'north',
+    rating : 4.3,
+    price: 90,
+    img: 'order/north1.png',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Stuffed Bati',
+    category : 'north',
+    rating : 4.3,
+    price: 55,
+    img: 'order/north2.png',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Samosa',
+    category : 'north',
+    rating : 4.3,
+    price: 30,
+    img: 'order/north3.png',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Dahi Bhalla',
+    category : 'north',
+    rating : 4.3,
+    price: 65,
+    img: 'order/north4.png',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Amritsari kulcha',
+    category : 'north',
+    rating : 4.3,
+    price: 89,
+    img: 'order/north5.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Dal Tarka',
+    category : 'north',
+    rating : 4.3,
+    price: 110,
+    img: 'order/north6.png',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Palak Panner',
+    category : 'north',
+    rating : 4.3,
+    price: 159,
+    img: 'order/north7.png',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 180,
+    img: 'order/dosa1.jpeg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 4.3,
+    price: 89,
+    img: 'order/south1.png',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 250,
+    img: 'order/dosa5.png',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mysore Bonda',
+    category : 'south indian',
+    rating : 4.3,
+    price: 89,
+    img: 'order/south2.png',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 169,
+    img: 'order/south3.png',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 99,
+    img: 'order/dosa4.png',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 179,
+    img: 'order/south4.png',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'south indian',
+    rating : 4.3,
+    price: 149,
+    img: 'order/south5.png',
+    quantity: 1
+},
+]
+
+export {foodItem};
